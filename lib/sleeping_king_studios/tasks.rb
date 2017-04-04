0db8d90da30c8015ddb3334102e894d9b25f90c5
sleepingkingstudios <merlin@sleepingkingstudios.com>
2017-04-04 17:54:29 -0400

    Implement SleepingKingStudios::Tasks::TaskGroup.

--- lib/sleeping_king_studios/tasks.rb
+++ lib/sleeping_king_studios/tasks.rb
@@ -5,6 +5,7 @@ module SleepingKingStudios
   # Toolkit providing an encapsulation layer around the Thor CLI library, with
   # predefined tasks for development and continuous integration.
   module Tasks
-    autoload :Task, 'sleeping_king_studios/tasks/task'
+    autoload :Task,      'sleeping_king_studios/tasks/task'
+    autoload :TaskGroup, 'sleeping_king_studios/tasks/task_group'
   end # module
 end # module
---
+++ lib/sleeping_king_studios/tasks/task_group.rb
@@ -0,0 +1,30 @@
+# lib/sleeping_king_studios/tasks/task_group.rb
+
+require 'thor'
+
+require 'sleeping_king_studios/tasks'
+
+module SleepingKingStudios::Tasks
+  # Exposes a group of encapsulated tasks with a shared namespace to Thor.
+  class TaskGroup < ::Thor
+    module ClassMethods # rubocop:disable Style/Documentation
+      # Adds a task definition to the task group. The task name, description,
+      # and method options (if any) are resolved from the task class, and a
+      # wrapper method is defined for calling the task.
+      def task definition, options = {}
+        task_name = options.fetch(:as, definition.task_name)
+
+        desc(task_name, definition.description)
+
+        definition.options.each do |option_name, option_params|
+          method_option option_name, option_params
+        end # each
+
+        define_method(task_name) do |*args|
+          definition.new(self.options).call(*args)
+        end # define_method
+      end # method task
+    end # module
+    extend ClassMethods
+  end # class
+end # module
