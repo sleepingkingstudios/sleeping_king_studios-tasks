edd10adbe400cd3368304610a779e2563ae0edb2
sleepingkingstudios <merlin@sleepingkingstudios.com>
2017-04-07 11:29:05 -0400

    Implement Tasks::Ci::RSpecRunner.

---
+++ lib/sleeping_king_studios/tasks/ci/rspec_runner.rb
@@ -0,0 +1,70 @@
+# lib/sleeping_king_studios/tasks/ci/rspec_runner.rb
+
+require 'json'
+
+require 'sleeping_king_studios/tasks/ci'
+
+module SleepingKingStudios::Tasks::Ci
+  # Service object to run RSpec as an external process with the specified
+  # parameters.
+  class RSpecRunner
+    # @param env [Hash] Environment variables to set for the RSpec process.
+    # @param options [Array] Options to pass to RSpec.
+    def initialize env: {}, options: []
+      @default_env     = env
+      @default_options = options
+    end # constructor
+
+    # @return [Hash] Environment variables to set for the RSpec process.
+    attr_reader :default_env
+
+    # @return [Array] Options to pass to RSpec.
+    attr_reader :default_options
+
+    def call env: {}, options: [], report: true
+      report  = 'tmp/ci/rspec.json' if report && !report.is_a?(String)
+      command =
+        build_command(:env => env, :options => options, :report => report)
+
+      stream_process(command)
+
+      report ? load_report(:report => report) : {}
+    end # method call
+
+    private
+
+    def build_command env:, options:, report:
+      env     = default_env.merge env
+      env     = env.map { |k, v| "#{tools.string.underscore(k).upcase}=#{v}" }
+      options = default_options + options
+      options << '--format=json' << "--out=#{report}" if report
+
+      "#{env.join ' '} bundle exec rspec #{options.join ' '}".strip
+    end # method build_command
+
+    def load_report report:
+      raw  = File.read report
+      json = JSON.parse raw
+
+      json['summary']
+    rescue
+      {}
+    end # method load_report
+
+    def stream_process command
+      Bundler.with_clean_env do
+        IO.popen(command) do |io|
+          loop do
+            char = io.getc
+
+            char ? print(char) : break
+          end # loop
+        end # popen
+      end # with_clean_env
+    end # method stream_process
+
+    def tools
+      SleepingKingStudios::Tools::Toolbelt.new
+    end # method tools
+  end # class
+end # module
---
+++ spec/sleeping_king_studios/tasks/ci/rspec_runner_spec.rb
@@ -0,0 +1,166 @@
+# spec/sleeping_king_studios/tasks/ci/rspec_runner_spec.rb
+
+require 'sleeping_king_studios/tasks/ci/rspec_runner'
+
+RSpec.describe SleepingKingStudios::Tasks::Ci::RSpecRunner do
+  let(:default_env)  { { :ci => true } }
+  let(:default_opts) { ['--color'] }
+  let(:instance) do
+    described_class.new :env => default_env, :options => default_opts
+  end # let
+
+  describe '::new' do
+    it 'should define the constructor' do
+      expect(described_class).
+        to be_constructible.
+        with(0).arguments.
+        and_keywords(:env, :options)
+    end # it
+  end # describe
+
+  describe '#call' do
+    let(:report) do
+      {
+        'duration'      => 1.0,
+        'example_count' => 3,
+        'failure_count' => 2,
+        'pending_count' => 1
+      } # end report
+    end # let
+    let(:report_file)      { 'tmp/ci/rspec.json' }
+    let(:expected_env)     { {} }
+    let(:expected_options) { ['--format=json', "--out=#{report_file}"] }
+    let(:expected_command) do
+      opts = default_opts + expected_options
+      env  = default_env.merge expected_env
+      env  = env.map { |k, v| "#{tools.string.underscore(k).upcase}=#{v}" }
+
+      "#{env.join ' '} bundle exec rspec #{opts.join ' '}".strip
+    end # let
+
+    def tools
+      SleepingKingStudios::Tools::Toolbelt.new
+    end # method tools
+
+    before(:example) do
+      allow(instance).to receive(:stream_process)
+
+      allow(File).to receive(:read).and_return('{}')
+    end # before example
+
+    it 'should define the method' do
+      expect(instance).
+        to respond_to(:call).
+        with(0).arguments.
+        and_keywords(:env, :options, :report)
+    end # it
+
+    it 'should call an rspec process' do
+      expect(instance).to receive(:stream_process).with(expected_command)
+
+      instance.call
+    end # it
+
+    it 'should load and parse the report file' do
+      expect(File).
+        to receive(:read).
+        with(report_file).
+        and_return(JSON.dump 'summary' => report)
+
+      expect(instance.call).to be == report
+    end # it
+
+    describe 'with :env => environment variables' do
+      let(:env)          { { :bundle_gemfile => 'path/to/Gemfile' } }
+      let(:expected_env) { super().merge env }
+
+      it 'should call an rspec process' do
+        expect(instance).to receive(:stream_process).with(expected_command)
+
+        instance.call :env => env
+      end # it
+    end # describe
+
+    describe 'with :options => custom options' do
+      let(:options)          { ['--format=progress'] }
+      let(:expected_options) { options + super() }
+
+      it 'should call an rspec process' do
+        expect(instance).to receive(:stream_process).with(expected_command)
+
+        instance.call :options => options
+      end # it
+    end # describe
+
+    describe 'with :report => false' do
+      let(:expected_options) { [] }
+
+      it 'should call an rspec process' do
+        expect(instance).to receive(:stream_process).with(expected_command)
+
+        instance.call :report => false
+      end # it
+
+      it 'should not load the report file' do
+        expect(File).not_to receive(:read)
+
+        expect(instance.call :report => false).to be == {}
+      end # it
+    end # describe
+
+    describe 'with :report => file path' do
+      let(:report_file) { 'tmp/rspec.json' }
+
+      it 'should load and parse the report file' do
+        expect(File).
+          to receive(:read).
+          with(report_file).
+          and_return(JSON.dump 'summary' => report)
+
+        expect(instance.call :report => report_file).to be == report
+      end # it
+    end # describe
+  end # describe
+
+  describe '#default_env' do
+    include_examples 'should have reader', :default_env, ->() { default_env }
+  end # describe
+
+  describe '#default_options' do
+    include_examples 'should have reader',
+      :default_options,
+      ->() { default_opts }
+  end # describe
+
+  describe '#stream_process' do
+    let(:io_stream) do
+      StringIO.new(
+        'What lies beyond the furthest reaches of the sky?'\
+        "\n"\
+        'That which will lead the lost child back to her mothers arms. Exile.'\
+        "\n"
+      ) # end stream
+    end # let
+    let(:command) { 'mysteria' }
+
+    it 'should define the private method' do
+      expect(instance).not_to respond_to(:stream_process)
+
+      expect(instance).to respond_to(:stream_process, true).with(1).argument
+    end # it
+
+    it 'should stream the output of the process' do
+      buffer = ''
+
+      allow(instance).to receive(:print) { |str| buffer << str }
+
+      expect(IO).
+        to receive(:popen).
+          with(command) { |_, &block| block.call(io_stream) }
+
+      instance.send :stream_process, command
+
+      expect(buffer).to be == io_stream.string
+    end # it
+  end # describe
+end # describe
