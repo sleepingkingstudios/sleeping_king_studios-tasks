6a153f6d62df0d2769669b1646b586b6d33d4ba8
sleepingkingstudios <merlin@sleepingkingstudios.com>
2017-04-25 13:51:39 -0400

    Delegate Tasks::Apps::Ci::RSpec to step runner.

--- lib/sleeping_king_studios/tasks/apps/ci/rspec.rb
+++ lib/sleeping_king_studios/tasks/apps/ci/rspec.rb
@@ -1,17 +1,11 @@
 # lib/sleeping_king_studios/tasks/apps/ci/rspec.rb
 
-require 'sleeping_king_studios/tasks/apps/applications_task'
 require 'sleeping_king_studios/tasks/apps/ci'
-require 'sleeping_king_studios/tasks/apps/ci/results_reporter'
-require 'sleeping_king_studios/tasks/ci/results_helpers'
-require 'sleeping_king_studios/tasks/ci/rspec_results'
-require 'sleeping_king_studios/tasks/ci/rspec_runner'
+require 'sleeping_king_studios/tasks/apps/ci/steps'
 
 module SleepingKingStudios::Tasks::Apps::Ci
   # Defines a Thor task for running the RSpec test suite for each application.
   class RSpec < SleepingKingStudios::Tasks::Task
-    include SleepingKingStudios::Tasks::Apps::ApplicationsTask
-
     def self.description
       'Runs the RSpec test suite for each application.'
     end # class method description
@@ -25,79 +19,11 @@ module SleepingKingStudios::Tasks::Apps::Ci
       :type    => :boolean,
       :default => false,
       :desc    => 'Do not write spec results to STDOUT.'
-    option :report,
-      :aliases => '-r',
-      :type    => :boolean,
-      :default => true,
-      :desc    => 'Write summary report to STDOUT.'
 
     def call *applications
-      mute! if quiet?
-
-      filtered = filter_applications :only => applications
-      results  = run_rspec(filtered)
-
-      return results unless report?
-
-      @mute = false
-
-      reporter = ResultsReporter.new(self)
-      reporter.call(results)
-
-      results
+      SleepingKingStudios::Tasks::Apps::Ci::Steps.
+        new(options.merge('only' => %w(rspec))).
+        call(*applications)
     end # method call
-
-    private
-
-    def aggregate_results results
-      totals = Hash.new { |hsh, key| hsh[key] = 0 }
-
-      results.each do |_, app_results|
-        rspec = app_results['RSpec']
-        rspec.to_h.each do |key, value|
-          totals[key] += value
-        end # each
-      end # each
-
-      totals = SleepingKingStudios::Tasks::Ci::RSpecResults.new(totals)
-
-      results['Totals']['RSpec'] = totals
-    end # method aggregate_results
-
-    def rspec_runner
-      opts = %w(--color --tty)
-      opts << '--format=documentation' unless quiet?
-
-      SleepingKingStudios::Tasks::Ci::RSpecRunner.new(:options => opts)
-    end # method rspec_runner
-
-    def run_rspec applications
-      results = Hash.new { |hsh, key| hsh[key] = {} }
-
-      applications.each do |name, app|
-        next if ci_step_config(name, :rspec) == false
-
-        results[name]['RSpec'] = run_rspec_for_application(name, app)
-      end # each
-
-      aggregate_results(results)
-
-      results
-    end # method run_rspec
-
-    def run_rspec_for_application name, config
-      say %(\nRunning specs for application "#{name}":)
-
-      spec_dir = spec_directories(name, config)
-
-      if spec_dir.empty?
-        return SleepingKingStudios::Tasks::Ci::RSpecResults.new({})
-      end # if
-
-      env = { :bundle_gemfile => config.fetch('gemfile', 'Gemfile') }
-      raw = rspec_runner.call(:env => env, :files => spec_dir)
-
-      SleepingKingStudios::Tasks::Ci::RSpecResults.new(raw)
-    end # method run_rspec_for_application
   end # class
 end # module
--- spec/sleeping_king_studios/tasks/apps/ci/rspec_spec.rb
+++ spec/sleeping_king_studios/tasks/apps/ci/rspec_spec.rb
@@ -21,213 +21,40 @@ RSpec.describe SleepingKingStudios::Tasks::Apps::Ci::RSpec do
   end # describe
 
   describe '#call' do
-    shared_examples 'should call an RSpec runner for each application' do
-      describe 'should call an RSpec runner for each application' do
-        before(:example) do
-          allow(instance).to receive(:applications).and_return(applications)
-
-          allow(SleepingKingStudios::Tasks::Ci::RSpecRunner).
-            to receive(:new).
-            with(:options => expected_options).
-            and_return(runner)
-
-          allow(SleepingKingStudios::Tasks::Apps::Ci::ResultsReporter).
-            to receive(:new).
-            with(instance).
-            and_return(reporter)
-        end # before example
+    shared_examples 'should call the step runner with only => "rspec"' do
+      describe 'should call the step runner with only => "rspec"' do
+        let(:expected_options) { options.merge 'only' => %w(rspec) }
+        let(:expected_results) { double('results') }
+        let(:step_runner) do
+          SleepingKingStudios::Tasks::Apps::Ci::Steps.new(expected_options)
+        end # let
 
         it do
-          expected_applications.each do |name, config|
-            gemfile      = config.fetch('gemfile', 'Gemfile')
-            expected_env = { :bundle_gemfile => gemfile }
+          expect(SleepingKingStudios::Tasks::Apps::Ci::Steps).
+            to receive(:new).
+            with(expected_options).
+            and_return(step_runner)
 
-            expect(instance).
-              to receive(:spec_directories).
-              with(name, config).
-              and_return(expected_files[name])
+          expect(step_runner).
+            to receive(:call).
+            with(*(only.empty? ? [no_args] : only)).
+            and_return(expected_results)
 
-            next if expected_files[name].empty?
-
-            expect(runner).
-              to receive(:call).
-              with(:env => expected_env, :files => expected_files[name]).
-              and_return(results[name]['RSpec'].to_h)
-          end # each
-
-          if options.fetch('report', true)
-            expect(reporter).to receive(:call).with(be == expected_results)
-          else
-            expect(reporter).not_to receive(:call)
-          end # if-else
-
-          expect(instance.call(*only)).to be == expected_results
+          expect(instance.call(*only)).to be expected_results
         end # it
       end # describe
     end # shared_examples
 
-    let(:applications) do
-      {
-        'admin'   => {},
-        'public'  => {},
-        'reports' => {}
-      } # end applications
-    end # let
-    let(:results) do
-      {
-        'admin' =>
-          {
-            'RSpec' =>
-              SleepingKingStudios::Tasks::Ci::RSpecResults.new(
-                'duration'      => 1.0,
-                'example_count' => 3,
-                'pending_count' => 1,
-                'failure_count' => 1
-              ) # end RSpec results
-          }, # end admin
-        'public' =>
-          {
-            'RSpec' =>
-              SleepingKingStudios::Tasks::Ci::RSpecResults.new(
-                'duration'      => 0.5,
-                'example_count' => 2,
-                'pending_count' => 1,
-                'failure_count' => 0
-              ) # end RSpec results
-          }, # end public
-        'reports' =>
-          {
-            'RSpec' =>
-              SleepingKingStudios::Tasks::Ci::RSpecResults.new(
-                'duration'      => 1.5,
-                'example_count' => 6,
-                'pending_count' => 0,
-                'failure_count' => 0
-              ) # end RSpec results
-          }, # end reports
-        'Totals' =>
-          {
-            'RSpec' =>
-              SleepingKingStudios::Tasks::Ci::RSpecResults.new(
-                'duration'      => 3.0,
-                'example_count' => 11,
-                'pending_count' => 2,
-                'failure_count' => 1
-              ) # end RSpec results
-          } # end totals
-      } # end results
-    end # let
     let(:only) { [] }
-    let(:expected_applications) { applications }
-    let(:expected_results)      { results }
-    let(:expected_options)      { ['--color', '--tty'] }
-    let(:expected_files) do
-      {
-        'admin'   => ['path/to/admin/spec'],
-        'public'  => ['path/to/public/spec'],
-        'reports' => ['path/to/reports/spec']
-      } # end applications
-    end # let
-    let(:reporter) do
-      SleepingKingStudios::Tasks::Apps::Ci::ResultsReporter.new(instance)
-    end # let
-    let(:runner) do
-      SleepingKingStudios::Tasks::Ci::RSpecRunner.
-        new(:options => expected_options)
-    end # let
-
-    before(:example) do
-      allow(instance).to receive(:applications).and_return(applications)
-    end # before example
 
     it { expect(instance).to respond_to(:call).with_unlimited_arguments }
 
-    include_examples 'should call an RSpec runner for each application'
+    include_examples 'should call the step runner with only => "rspec"'
 
     describe 'with applications' do
       let(:only) { %w(public) }
-      let(:expected_applications) do
-        applications.select { |key, _| only.include?(key) }
-      end # let
-      let(:expected_results) do
-        hsh =
-          results.select { |key, _| [*only, 'Totals'].include?(key) }
 
-        hsh['Totals'] = hsh['public']
-
-        hsh
-      end # let
-
-      include_examples 'should call an RSpec runner for each application'
+      include_examples 'should call the step runner with only => "rspec"'
     end # describe
-
-    describe 'with :report => false' do
-      let(:options) { super().merge 'report' => false }
-
-      include_examples 'should call an RSpec runner for each application'
-    end # describe
-
-    context 'when an application disables the rspec step' do
-      let(:applications) do
-        hsh = super()
-
-        (hsh['public']['ci'] = {})['rspec'] = false
-
-        hsh
-      end # let
-      let(:expected_applications) do
-        applications.reject { |key, _| key == 'public' }
-      end # let
-      let(:expected_results) do
-        hsh = results.dup
-
-        hsh.delete('public')
-
-        hsh['Totals'] =
-          {
-            'RSpec' =>
-              SleepingKingStudios::Tasks::Ci::RSpecResults.new(
-                'duration'      => 2.5,
-                'example_count' => 9,
-                'failure_count' => 1,
-                'pending_count' => 1
-              ) # end RSpec results
-          } # end totals
-
-        hsh
-      end # let
-
-      include_examples 'should call an RSpec runner for each application'
-    end # context
-
-    context 'when an application does not have spec files' do
-      let(:expected_files) do
-        {
-          'admin'   => ['path/to/admin/spec'],
-          'public'  => [],
-          'reports' => ['path/to/reports/spec']
-        } # end applications
-      end # let
-      let(:expected_results) do
-        hsh = results.dup
-
-        hsh['public']['RSpec'] =
-          SleepingKingStudios::Tasks::Ci::RSpecResults.new({})
-        hsh['Totals'] =
-          {
-            'RSpec' =>
-              SleepingKingStudios::Tasks::Ci::RSpecResults.new(
-                'duration'      => 2.5,
-                'example_count' => 9,
-                'failure_count' => 1,
-                'pending_count' => 1
-              ) # end RSpec results
-          } # end totals
-
-        hsh
-      end # let
-
-      include_examples 'should call an RSpec runner for each application'
-    end # context
   end # describe
 end # describe
