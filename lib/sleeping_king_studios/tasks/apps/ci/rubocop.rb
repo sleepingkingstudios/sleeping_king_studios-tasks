c04abf63194ad8d890f9ca733f6185e933683c53
sleepingkingstudios <merlin@sleepingkingstudios.com>
2017-04-25 14:00:17 -0400

    Delegate Tasks::Apps::Ci::RuboCop to step runner.

--- lib/sleeping_king_studios/tasks/apps/ci/rubocop.rb
+++ lib/sleeping_king_studios/tasks/apps/ci/rubocop.rb
@@ -1,16 +1,11 @@
 # lib/sleeping_king_studios/tasks/apps/ci/rubocop.rb
 
-require 'sleeping_king_studios/tasks/apps/applications_task'
 require 'sleeping_king_studios/tasks/apps/ci'
-require 'sleeping_king_studios/tasks/apps/ci/results_reporter'
-require 'sleeping_king_studios/tasks/ci/rubocop_results'
-require 'sleeping_king_studios/tasks/ci/rubocop_runner'
+require 'sleeping_king_studios/tasks/apps/ci/steps'
 
 module SleepingKingStudios::Tasks::Apps::Ci
   # Defines a Thor task for running the RuboCop linter for each application.
   class RuboCop < SleepingKingStudios::Tasks::Task
-    include SleepingKingStudios::Tasks::Apps::ApplicationsTask
-
     def self.description
       'Runs the RuboCop linter for each application.'
     end # class method description
@@ -24,79 +19,11 @@ module SleepingKingStudios::Tasks::Apps::Ci
       :type    => :boolean,
       :default => false,
       :desc    => 'Do not write lint results to STDOUT.'
-    option :report,
-      :aliases => '-r',
-      :type    => :boolean,
-      :default => true,
-      :desc    => 'Write summary report to STDOUT.'
 
     def call *applications
-      mute! if quiet?
-
-      filtered = filter_applications :only => applications
-      results  = run_rubocop(filtered)
-
-      return results unless report?
-
-      @mute = false
-
-      reporter = ResultsReporter.new(self)
-      reporter.call(results)
-
-      results
+      SleepingKingStudios::Tasks::Apps::Ci::Steps.
+        new(options.merge('only' => %w(rubocop))).
+        call(*applications)
     end # method call
-
-    private
-
-    def aggregate_results results
-      totals = Hash.new { |hsh, key| hsh[key] = 0 }
-
-      results.each do |_, app_results|
-        rubocop = app_results['RuboCop']
-
-        totals['inspected_file_count'] += rubocop.inspected_file_count
-        totals['offense_count']        += rubocop.offense_count
-      end # each
-
-      totals = SleepingKingStudios::Tasks::Ci::RuboCopResults.new(totals)
-
-      results['Totals']['RuboCop'] = totals
-    end # method aggregate_results
-
-    def rubocop_runner
-      opts = %w(--color)
-      opts << '--format=progress' unless quiet?
-
-      SleepingKingStudios::Tasks::Ci::RuboCopRunner.new(:options => opts)
-    end # method rubocop_runner
-
-    def run_rubocop applications
-      results = Hash.new { |hsh, key| hsh[key] = {} }
-
-      applications.each do |name, app|
-        next if ci_step_config(name, :rubocop) == false
-
-        results[name]['RuboCop'] = run_rubocop_for_application(name, app)
-      end # each
-
-      aggregate_results(results)
-
-      results
-    end # method run_rubocop
-
-    def run_rubocop_for_application name, config
-      gemfile   = config.fetch('gemfile', 'Gemfile')
-      src_files =
-        source_files(name, config) +
-        spec_directories(name, config)
-
-      say %(\nRunning linter for application "#{name}":)
-
-      env = { :bundle_gemfile => gemfile }
-
-      raw = rubocop_runner.call(:env => env, :files => src_files)
-
-      SleepingKingStudios::Tasks::Ci::RuboCopResults.new(raw)
-    end # method run_rspec_for_application
   end # class
 end # module
--- spec/sleeping_king_studios/tasks/apps/ci/rubocop_spec.rb
+++ spec/sleeping_king_studios/tasks/apps/ci/rubocop_spec.rb
@@ -21,165 +21,40 @@ RSpec.describe SleepingKingStudios::Tasks::Apps::Ci::RuboCop do
   end # describe
 
   describe '#call' do
-    shared_examples 'should call a RuboCop runner for each application' do
-      describe 'should call an RuboCop runner for each application' do
-        before(:example) do
-          allow(instance).to receive(:applications).and_return(applications)
-
-          allow(SleepingKingStudios::Tasks::Ci::RuboCopRunner).
-            to receive(:new).
-            with(:options => expected_options).
-            and_return(runner)
-
-          allow(SleepingKingStudios::Tasks::Apps::Ci::ResultsReporter).
-            to receive(:new).
-            with(instance).
-            and_return(reporter)
-        end # before example
+    shared_examples 'should call the step runner with only => "rubocop"' do
+      describe 'should call the step runner with only => "rubocop"' do
+        let(:expected_options) { options.merge 'only' => %w(rubocop) }
+        let(:expected_results) { double('results') }
+        let(:step_runner) do
+          SleepingKingStudios::Tasks::Apps::Ci::Steps.new(expected_options)
+        end # let
 
         it do
-          expected_applications.each do |name, config|
-            gemfile        = config.fetch('gemfile', 'Gemfile')
-            expected_env   = { :bundle_gemfile => gemfile }
-            expected_files = ["path/to/#{name}/spec"]
+          expect(SleepingKingStudios::Tasks::Apps::Ci::Steps).
+            to receive(:new).
+            with(expected_options).
+            and_return(step_runner)
 
-            expect(instance).
-              to receive(:spec_directories).
-              with(name, config).
-              and_return(expected_files)
+          expect(step_runner).
+            to receive(:call).
+            with(*(only.empty? ? [no_args] : only)).
+            and_return(expected_results)
 
-            expect(runner).
-              to receive(:call).
-              with(:env => expected_env, :files => expected_files).
-              and_return(results[name]['RuboCop'].to_h)
-          end # each
-
-          if options.fetch('report', true)
-            expect(reporter).to receive(:call).with(expected_results)
-          else
-            expect(reporter).not_to receive(:call)
-          end # if
-
-          expect(instance.call(*only)).to be == expected_results
+          expect(instance.call(*only)).to be expected_results
         end # it
       end # describe
     end # shared_examples
 
-    let(:applications) do
-      {
-        'admin'   => {},
-        'public'  => {},
-        'reports' => {}
-      } # end applications
-    end # let
-    let(:results) do
-      {
-        'admin' =>
-          {
-            'RuboCop' =>
-              SleepingKingStudios::Tasks::Ci::RuboCopResults.new(
-                'inspected_file_count' => 5,
-                'offense_count'        => 3
-              ) # end RuboCop results
-          }, # end admin
-        'public' =>
-          {
-            'RuboCop' =>
-              SleepingKingStudios::Tasks::Ci::RuboCopResults.new(
-                'inspected_file_count' => 10,
-                'offense_count'        => 1
-              ) # end RuboCop results
-          }, # end public
-        'reports' =>
-          {
-            'RuboCop' =>
-              SleepingKingStudios::Tasks::Ci::RuboCopResults.new(
-                'inspected_file_count' => 15,
-                'offense_count'        => 0
-              ) # end RuboCop results
-          }, # end reports
-        'Totals' =>
-          {
-            'RuboCop' =>
-              SleepingKingStudios::Tasks::Ci::RuboCopResults.new(
-                'inspected_file_count' => 30,
-                'offense_count'        => 4
-              ) # end RuboCop results
-          } # end totals
-      } # end results
-    end # let
     let(:only) { [] }
-    let(:expected_applications) { applications }
-    let(:expected_results)      { results }
-    let(:expected_options)      { ['--color'] }
-    let(:reporter) do
-      SleepingKingStudios::Tasks::Apps::Ci::ResultsReporter.new(instance)
-    end # let
-    let(:runner) do
-      SleepingKingStudios::Tasks::Ci::RuboCopRunner.
-        new(:options => expected_options)
-    end # let
-
-    before(:example) do
-      allow(instance).to receive(:applications).and_return(applications)
-    end # before example
 
     it { expect(instance).to respond_to(:call).with_unlimited_arguments }
 
-    include_examples 'should call a RuboCop runner for each application'
+    include_examples 'should call the step runner with only => "rubocop"'
 
     describe 'with applications' do
       let(:only) { %w(public) }
-      let(:expected_applications) do
-        applications.select { |key, _| only.include?(key) }
-      end # let
-      let(:expected_results) do
-        hsh =
-          results.select { |key, _| [*only, 'Totals'].include?(key) }
 
-        hsh['Totals'] = hsh['public']
-
-        hsh
-      end # let
-
-      include_examples 'should call a RuboCop runner for each application'
+      include_examples 'should call the step runner with only => "rubocop"'
     end # describe
-
-    describe 'with :report => false' do
-      let(:options) { super().merge 'report' => false }
-
-      include_examples 'should call a RuboCop runner for each application'
-    end # describe
-
-    context 'when an application disables the rspec step' do
-      let(:applications) do
-        hsh = super()
-
-        (hsh['public']['ci'] = {})['rubocop'] = false
-
-        hsh
-      end # let
-      let(:expected_applications) do
-        applications.reject { |key, _| key == 'public' }
-      end # let
-      let(:expected_results) do
-        hsh = results.dup
-
-        hsh.delete('public')
-
-        hsh['Totals'] =
-          {
-            'RuboCop' =>
-              SleepingKingStudios::Tasks::Ci::RuboCopResults.new(
-                'inspected_file_count' => 20,
-                'offense_count'        => 3
-              ) # end RSpec results
-          } # end totals
-
-        hsh
-      end # let
-
-      include_examples 'should call a RuboCop runner for each application'
-    end # context
   end # describe
 end # describe
